c422a8fb614661c023e8dbe57c268ce666cdd482
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-29 21:30:49 +0300

    minor sketch changes

--- src/MicroPython/ESP-32 Climate Station/main.py
+++ src/MicroPython/ESP-32 Climate Station/main.py
@@ -1,17 +1,18 @@
-def connect_to_rabbit():
-    print('Connect to Rabbit')
+def connect_to_rabbit(client_name, broker_host, keep_alive):
+    try:
+        print('Connect to Rabbit.')
 
-    client_name = conf['mqtt_client']
-    broker_host = conf['mqtt_host']
-    keep_alive = conf['keep_alive']
-    mqttc = MQTTClient(client_name, broker_host, keepalive=keep_alive)
-    mqttc.connect()
+        mqttc = MQTTClient(client_name, broker_host, keepalive=keep_alive)
+        mqttc.connect()
+        
+        print('Connected to Rabbit.')
+        return mqttc
+    except OSError as e:
+        print('Failed to connect.')
+        return None
     
-    print('Connected to Rabbit')
-    return mqttc
-    
-def defy_sensor_pin():
-    return dht.DHT22(Pin(conf['sensor_pin']))
+def defy_sensor_pin(pin):
+    return dht.DHT22(Pin(pin))
 
 def temperature():
     try:
@@ -27,14 +28,23 @@ def temperature():
         print('Failed to read sensor.')
         return False, 0, 0
     
-sensor = defy_sensor_pin()
-mqttc = connect_to_rabbit()
+room_id = conf['room_id']
+sensor_delay = conf['sensor_delay']
+
+sensor_pin = conf['sensor_pin']
+sensor = defy_sensor_pin(sensor_pin)
+
+client_name = conf['mqtt_client']
+broker_host = conf['mqtt_host']
+keep_alive = conf['keep_alive']
+mqttc = connect_to_rabbit(client_name, broker_host, keep_alive)
+
 while True:
 	status, tmp, hum = temperature()
 	if status:
-         now = get_time()
-         json = '{ "roomId": 1, "temperature": %.2f, "humidity": %.2f, "measurmentTime": "%s" }' % (tmp, hum, str(get_time()))
-         mqttc.publish('climate-measured-event', json)
-         print(json)
-         sleep(conf['sensor_delay'])
+    	now = get_time()
+    	json = '{ "roomId": %i, "temperature": %.2f, "humidity": %.2f, "measurmentTime": "%s" }' % (room_id, tmp, hum, str(get_time()))
+    	mqttc.publish('climate-measured-event', json)
+    	print(json)
+    	sleep(sensor_delay)
 
